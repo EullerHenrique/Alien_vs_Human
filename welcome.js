99b41d5d4ae72116baa74623db0b75d392cae078
EullerHenrique <48317736+EullerHenrique@users.noreply.github.com>
2020-02-03 04:18:56 -0300

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -2,6 +2,6 @@ function welcome() {
     //Reproduz o áudio de boas vindas
 
     var audio_welcome = new Audio();
-    audio_welcome.src = '../MP3/welcome.mp3';
+    audio_welcome.src = 'welcome.mp3';
     audio_welcome.play();
 }
