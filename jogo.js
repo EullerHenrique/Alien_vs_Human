d9d107f2b57c2bedefc2ae7af908590bb6222e39
EullerHenrique <48317736+EullerHenrique@users.noreply.github.com>
2020-02-04 22:42:49 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -194,9 +194,9 @@
                         criaFogueteTempo = 800;
                         vidas = 200;
                     }
-            }
+            
             console.log('Velocidade do jogo escolhida', criaFogueteTempo);
-         }
+         
         //
 
         //Recebe o tempo de jogo escolhido
